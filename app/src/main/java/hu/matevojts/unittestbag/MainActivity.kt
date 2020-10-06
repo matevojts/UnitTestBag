73a1ae2ce05918f9c5c1394eb418fe093bb8f9de
Máté Vojts <mate.vojts@gmail.com>
2020-10-06 22:29:19 +0200

    feat: initialize project

---
+++ app/src/main/java/hu/matevojts/unittestbag/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.matevojts.unittestbag
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
