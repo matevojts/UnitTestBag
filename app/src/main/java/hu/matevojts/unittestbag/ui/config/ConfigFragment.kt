f04cf1fbc62ac065228b6148c2d0a9f0bc69fbbf
Máté Vojts <mate.vojts@gmail.com>
2020-10-21 13:54:33 +0200

    feat: add basic error handling to ConfigFragment

--- app/src/main/java/hu/matevojts/unittestbag/ui/config/ConfigFragment.kt
+++ app/src/main/java/hu/matevojts/unittestbag/ui/config/ConfigFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import hu.matevojts.unittestbag.R
 import hu.matevojts.unittestbag.databinding.FragmentConfigBinding
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
@@ -29,6 +31,12 @@ class ConfigFragment : Fragment() {
 
         viewModel.output.openBag
             .subscribe { this.findNavController().navigate(ConfigFragmentDirections.actionToOpenedBagFragment()) }
+            .addTo(foregroundDisposables)
+
+        viewModel.output.invalidItems
+            .subscribe {
+                Toast.makeText(requireContext(), R.string.config_empty_bag_error_message, Toast.LENGTH_SHORT).show()
+            }
             .addTo(foregroundDisposables)
     }
 
